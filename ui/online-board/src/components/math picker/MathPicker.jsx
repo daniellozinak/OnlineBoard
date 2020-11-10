da5737c0cfd8e5e3c032cda07e983b6ff4a7907c
daniellozinak <danolozinak@gmail.com>
2020-11-10 17:42:30 +0100

    Added math keyboard

--- ui/online-board/src/components/math picker/MathPicker.jsx
+++ ui/online-board/src/components/math picker/MathPicker.jsx
@@ -31,15 +31,18 @@ class MathPicker extends React.Component{
     }
 
     _onChange = e =>{
-        if(this.math_field == null)
+        if(this.math_field == null || e.latex() === "undefined")
         {
             this.math_field = e;
-           this.math_field.latex("");
+            this.math_field.latex("");
+            console.log("init");
+            return;
         }
+        //this.math_field.latex("");
         this.setState({text:e.latex()});
-        //console.log(e);
+        this.math_field.focus();
 
-        console.log(e.text());
+        console.log("_onchange: " + e.latex());
     }
 
     _onAddSign(sign){
@@ -51,6 +54,10 @@ class MathPicker extends React.Component{
     _onMove(dir)
     {
         this.math_field.keystroke(dir);
+    }
+
+    clear = e =>{
+        this.math_field.latex("");
     }
 
     _onDispatchEvent =e =>{
@@ -68,13 +75,27 @@ class MathPicker extends React.Component{
                 <button onClick={this._onClick.bind(this)} className="button">Math</button>
                 {this.state.show &&
                     <div className="math-panel-wrapper">
-                        <div className="math-panel-buttons">
-                            <button onClick={()=>this._onAddSign("∫")}>∫</button>
-                            <button onClick={()=>this._onAddSign("∑")}>∑</button>
-                            <button onClick={()=>this._onMove(Direction.UP)}>up</button>
-                            <button onClick={()=>this._onMove(Direction.DOWN)}>down</button>
-                            <button onClick={()=>this._onMove(Direction.RIGHT)}>right</button>
-                            <button onClick={()=>this._onMove(Direction.LEFT)}>left</button>
+                        <div className="math-panel-keyboard">
+                            <button onClick={()=>this._onAddSign("\\int_{ }^{ }")}>∫</button>
+                            <button onClick={()=>this._onAddSign("\\sum_{ }^{ }")}>∑</button>
+                            <button onClick={()=>this._onAddSign("\\log_{}")}>Log</button>
+                            <button onClick={()=>this._onAddSign("\\lim_{x\\to }")}>Lim</button>
+                            <button onClick={()=>this._onAddSign("\\^{}")}>^x</button>
+                            <button onClick={()=>this._onAddSign("\\sqrt[]{}")}>sqrt</button>
+                            <button onClick={()=>this._onAddSign("\\prod_{}^{}")}>∏</button>
+                            <button onClick={()=>this._onAddSign("\\cdot")}>dot</button>
+                            <button onClick={()=>this._onAddSign("\\pm")}>±</button> 
+                            <button onClick={()=>this._onAddSign("\\frac{}{}")}>x/y</button> 
+                            <button onClick={()=>this._onAddSign("\\times")}>cross</button> 
+                            <button onClick={()=>this._onAddSign("\\infinity")}>∞</button> 
+                            <button onClick={()=>this._onAddSign("\\infinity")}>pi</button> 
+                            <div className="math-panel-keyboard-control">
+                                <button className="button-up" onClick={()=>this._onMove(Direction.UP)}>↑</button>
+                                <button className="button-down" onClick={()=>this._onMove(Direction.DOWN)}>↓</button>
+                                <button className="button-right" onClick={()=>this._onMove(Direction.RIGHT)}>→</button>
+                                <button className="button-left" onClick={()=>this._onMove(Direction.LEFT)}>←</button>
+                            </div>
+                            <button onClick={()=>this.clear()}>clear</button>
                         </div>
                         <div className="root-input-wrapper">
                         <EditableMathField
